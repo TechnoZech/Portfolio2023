50b02bc8d028cd2e9ea9fb8504c157f08af35518
unknown <sharmadharmesh1998@gmail.com>
2023-06-02 00:31:14 +0530

    leading effect added

--- script.js
+++ script.js
@@ -38,3 +38,20 @@ function mobileNavClose(e){
     mobileNav.classList.remove("active_mob_nav");
 
 }
+
+// ================================
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        // console.log(entry);
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }else{
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
+
